ce9789f92cb8aaddd7dfd0e07b9ffe53f489cdd0
안민현 <minhyun17@naver.com>
2023-06-28 22:03:11 +0900

    init commit

---
+++ src/main/java/com/sparta/levelone/LevelOneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.levelone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelOneApplication.class, args);
+    }
+
+}
