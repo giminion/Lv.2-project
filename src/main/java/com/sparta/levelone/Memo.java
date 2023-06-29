ad413851b0efa2b370f8d0dab6660059dbcc6a51
안민현 <minhyun17@naver.com>
2023-06-29 15:20:56 +0900

    Controller 이해 및 CRUD 메서드 기본 활용, Getter와 Setter 알아보기

--- src/main/java/com/sparta/levelone/Memo.java
+++ src/main/java/com/sparta/levelone/Memo.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 @Getter
 @Setter
-@RequiredArgsConstructor // final 파라미터만 들어간 생성자 만들어줌
-@AllArgsConstructor // 모든 파라미터 들어간 생성자 만듬
-@NoArgsConstructor // 기본생성자 만듬
 public class Memo {
     private String username;
     private String contents;
--- src/main/java/com/sparta/levelone/controller/BlogController.java
+++ src/main/java/com/sparta/levelone/controller/BlogController.java
@@ -1,15 +1,28 @@
 package com.sparta.levelone.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class BlogController {
 
-    @GetMapping("/blog")
-    public String hello(){
-        return "Hello World!";
+    @GetMapping("/blog")  // 게시물 조회
+    public String getMemo(){
+        return "게시물 조회";
+    }
+
+    @PostMapping("/blog")  // 게시물 작성
+    public String postmemo(){
+        return "게시물 작성";
+    }
+
+    @PutMapping("/blog")
+    public String putmemo(){
+        return "PUT 메서드";
+    }
+
+    @DeleteMapping("/blog")  // 게시물 삭제
+    public String deleteMemo(){
+        return "게시물 삭제";
     }
 }
