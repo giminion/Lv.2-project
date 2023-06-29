4ac23c3155d8a6757103957683f6f747b953605f
안민현 <minhyun17@naver.com>
2023-06-29 19:51:48 +0900

    init

--- src/main/java/com/sparta/levelone/Memo.java
+++ src/main/java/com/sparta/levelone/Memo.java
@@ -1,18 +1,5 @@
 package com.sparta.levelone;
 
-import lombok.*;
-
-@Getter
-@Setter
 public class Memo {
-    private String username;
-    private String contents;
-}
 
-class Main{
-    public static void main(String[] args) {
-        Memo memo = new Memo();
-        memo.setUsername("Robbie");
-        System.out.println(memo.getUsername());
-    }
 }
