9a183596578132ef9c61a29067cee0e183c4ef0b
안민현 <minhyun17@naver.com>
2023-06-30 18:19:44 +0900

    3Layers 분리

--- src/main/java/com/sparta/levelone/controller/BlogController.java
+++ src/main/java/com/sparta/levelone/controller/BlogController.java
@@ -1,28 +1,44 @@
 package com.sparta.levelone.controller;
 
+import com.sparta.levelone.dto.BlogRequestDto;
+import com.sparta.levelone.dto.BlogResponseDto;
+import com.sparta.levelone.service.BlogService;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class BlogController {
 
-    @GetMapping("/blog")  // 게시물 조회
-    public String getMemo(){
-        return "게시물 조회";
+    private final JdbcTemplate jdbcTemplate;
+
+    public BlogController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    @PostMapping("/blog")  // 게시물 작성
-    public String postmemo(){
-        return "게시물 작성";
+    @PostMapping("/blog")
+    public BlogResponseDto createBlog(@RequestBody BlogRequestDto requestDto) {
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.createBlog(requestDto);
     }
 
-    @PutMapping("/blog")
-    public String putmemo(){
-        return "PUT 메서드";
+    @GetMapping("/blog")
+    public List<BlogResponseDto> getBlog() {
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.getBlog();
     }
 
-    @DeleteMapping("/blog")  // 게시물 삭제
-    public String deleteMemo(){
-        return "게시물 삭제";
+    @PutMapping("/blog/{id}")
+    public Long updateBlog(@PathVariable Long id, @RequestBody BlogRequestDto requestDto) {
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.updateBlog(id,requestDto);
+    }
+
+    @DeleteMapping("/blog/{id}")
+    public Long deleteBlog(@PathVariable Long id) {
+        BlogService blogService = new BlogService(jdbcTemplate);
+        return blogService.deleteBlog(id);
     }
 }
---
+++ src/main/java/com/sparta/levelone/service/BlogService.java
@@ -0,0 +1,67 @@
+package com.sparta.levelone.service;
+
+import com.sparta.levelone.dto.BlogRequestDto;
+import com.sparta.levelone.dto.BlogResponseDto;
+import com.sparta.levelone.entity.Blog;
+import com.sparta.levelone.repository.BlogRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class BlogService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BlogService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public BlogResponseDto createBlog(BlogRequestDto requestDto) {
+        // RequestDto -> Entity
+        Blog blog = new Blog(requestDto);
+
+        BlogRepository blogRepository = new BlogRepository(jdbcTemplate);
+        Blog saveBlog = blogRepository.save(blog);
+
+        // Entity -> ResponseDto
+        BlogResponseDto blogResponseDto = new BlogResponseDto(blog);
+
+        return blogResponseDto;
+    }
+
+    public List<BlogResponseDto> getBlog() {
+        // DB 조회
+        BlogRepository blogRepository = new BlogRepository(jdbcTemplate);
+        return blogRepository.findAll();
+    }
+
+    public Long updateBlog(Long id, BlogRequestDto requestDto) {
+        BlogRepository blogRepository = new BlogRepository(jdbcTemplate);
+        // 해당 메모가 DB에 존재하는지 확인
+        Blog blog = blogRepository.findById(id);
+        if(blog != null) {
+            // memo 내용 수정
+            blogRepository.update(id,requestDto);
+
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 게시물은 존재하지 않습니다.");
+        }
+    }
+
+    public Long deleteBlog(Long id) {
+        BlogRepository blogRepository = new BlogRepository(jdbcTemplate);
+        // 해당 메모가 DB에 존재하는지 확인
+        Blog blog = blogRepository.findById(id);
+        if(blog != null) {
+            // memo 삭제
+            blogRepository.delete(id);
+
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 게시물은 존재하지 않습니다.");
+        }
+    }
+
+
+}
